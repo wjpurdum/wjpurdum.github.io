e482a918a878b08bb32e8cd169ac5fbb05389f9e
Whitney <whitney.purdum@gmail.com>
2017-05-18 14:20:39 -0400

    reconfigure html and css

--- scripts.js
+++ scripts.js
@@ -1,18 +1,6 @@
 $(document).ready(function() {
     
-	$("#skills-link").click(function(){
-    	$(".skills").fadeIn(3000).addClass("selected");
-    	if ($(".contact").hasClass('selected') === true) {
-    		$(".contact").hide();
-    	}
-});
 
-	$("#contact-link").click(function() {
-		$(".contact").fadeIn(3000).addClass("selected");
-		if ($(".skills").hasClass('selected') === true) {
-			$(".skills").hide();
-		}
-	});
 
 
 });
